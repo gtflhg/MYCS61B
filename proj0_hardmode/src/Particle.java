aa1f4fab42f0537311e0a9d2699cb2c501d88467
lhg <a1030250365@sjtu.edu.cn>
2026-03-13 00:02:11 +0800

    Finish Task1-3

--- proj0_hardmode/src/Particle.java
+++ proj0_hardmode/src/Particle.java
@@ -1,2 +1,34 @@
+import java.util.Map;
+import java.awt.Color;
+
 public class Particle {
+    public static final int PLANT_LIFESPAN = 150;
+    public static final int FLOWER_LIFESPAN = 75;
+    public static final int FIRE_LIFESPAN = 10;
+    public static final Map<ParticleFlavor, Integer> LIFESPANS =
+            Map.of(ParticleFlavor.FLOWER, FLOWER_LIFESPAN,
+                    ParticleFlavor.PLANT, PLANT_LIFESPAN,
+                    ParticleFlavor.FIRE, FIRE_LIFESPAN);
+    public static final Map<ParticleFlavor, Color> COLORS =
+            Map.of(ParticleFlavor.EMPTY, Color.BLACK,
+                    ParticleFlavor.SAND, Color.YELLOW,
+                    ParticleFlavor.BARRIER, Color.GRAY,
+                    ParticleFlavor.WATER, Color.BLUE,
+                    ParticleFlavor.FOUNTAIN, Color.CYAN,
+                    ParticleFlavor.PLANT, new Color(0, 255, 0),
+                    ParticleFlavor.FIRE, new Color(255, 0, 0),
+                    ParticleFlavor.FLOWER, new Color(255, 141, 161)
+            );
+
+    ParticleFlavor flavor;
+    int lifespan;
+
+    public Particle(ParticleFlavor flavor) {
+        this.flavor = flavor;
+        this.lifespan = LIFESPANS.getOrDefault(flavor, -1);
+    }
+
+    public Color color(){
+        return COLORS.get(flavor);
+    }
 }
