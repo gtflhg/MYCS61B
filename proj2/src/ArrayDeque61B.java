fa401f24555b1b6d12b76e854e0dae5c7f9661d3
lhg <a1030250365@sjtu.edu.cn>
2026-03-17 23:43:53 +0800

    Finish Task1-7

---
+++ proj2/src/ArrayDeque61B.java
@@ -0,0 +1,193 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayDeque61B<T> implements Deque61B<T>{
+    private int size;
+    private T[] elements;
+    private int nextFirst;
+
+    public ArrayDeque61B() {
+        size = 0;
+        nextFirst = 0;
+        elements = (T[]) new Object[1000];
+    }
+
+    private int getActualIndex(int index){
+        int actualIndex = index + nextFirst + 1;
+        if(actualIndex >= 1000){
+            actualIndex -= 1000;
+        }
+        return actualIndex;
+    }
+
+    private int update(int i, int toUpdate){
+        int index = toUpdate + i;
+        if(index < 0){
+            index += 1000;
+        }else if(index >= 1000){
+            index -= 1000;
+        }
+        return index;
+    }
+
+    private int getNextLast(){
+        if(size == 0){
+            return 0;
+        }
+        return update(size + 1, nextFirst);
+    }
+
+    private boolean validateIndex(int index){
+        return index >= 0 && index < size;
+    }
+
+    /**
+     * Add {@code x} to the front of the deque. Assumes {@code x} is never null.
+     *
+     * @param x item to add
+     */
+    @Override
+    public void addFirst(T x) {
+        int index = getActualIndex(-1);
+        elements[index] = x;
+        size++;
+        nextFirst = update(-1, nextFirst);
+    }
+
+    /**
+     * Add {@code x} to the back of the deque. Assumes {@code x} is never null.
+     *
+     * @param x item to add
+     */
+    @Override
+    public void addLast(T x) {
+        int index = getActualIndex(size);
+        elements[index] = x;
+        size++;
+        if (index == 0){
+            nextFirst = update(-1, nextFirst);
+        }
+    }
+
+    /**
+     * Returns a List copy of the deque. Does not alter the deque.
+     *
+     * @return a new list copy of the deque.
+     */
+    @Override
+    public List<T> toList() {
+        List<T> returnList = new ArrayList<>();
+        for (int i = 0; i < size; i++){
+            returnList.add(elements[getActualIndex(i)]);
+        }
+        return returnList;
+    }
+
+    /**
+     * Returns if the deque is empty. Does not alter the deque.
+     *
+     * @return {@code true} if the deque has no elements, {@code false} otherwise.
+     */
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * Returns the size of the deque. Does not alter the deque.
+     *
+     * @return the number of items in the deque.
+     */
+    @Override
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Return the element at the front of the deque, if it exists.
+     *
+     * @return element, otherwise {@code null}.
+     */
+    @Override
+    public T getFirst() {
+        return elements[getActualIndex(0)];
+    }
+
+    /**
+     * Return the element at the back of the deque, if it exists.
+     *
+     * @return element, otherwise {@code null}.
+     */
+    @Override
+    public T getLast() {
+        return elements[getActualIndex(size-1)];
+    }
+
+    /**
+     * Remove and return the element at the front of the deque, if it exists.
+     *
+     * @return removed element, otherwise {@code null}.
+     */
+    @Override
+    public T removeFirst() {
+        if(size == 0){
+            return null;
+        }
+        T temp = elements[getActualIndex(0)];
+        elements[getActualIndex(0)] = null;
+        size--;
+        nextFirst = update(1,  nextFirst);
+        return temp;
+    }
+
+    /**
+     * Remove and return the element at the back of the deque, if it exists.
+     *
+     * @return removed element, otherwise {@code null}.
+     */
+    @Override
+    public T removeLast() {
+        if (size == 0){
+            return null;
+        }
+        T temp = elements[getActualIndex(size-1)];
+        elements[getActualIndex(size-1)] = null;
+        size--;
+        if (size == 0){
+            nextFirst = update(1, nextFirst);
+        }
+        return temp;
+    }
+
+    /**
+     * The Deque61B abstract data type does not typically have a get method,
+     * but we've included this extra operation to provide you with some
+     * extra programming practice. Gets the element, iteratively. Returns
+     * null if index is out of bounds. Does not alter the deque.
+     *
+     * @param index index to get
+     * @return element at {@code index} in the deque
+     */
+    @Override
+    public T get(int index) {
+        if (!validateIndex(index)||size==0){
+            return null;
+        }
+        return elements[getActualIndex(index)];
+    }
+
+    /**
+     * This method technically shouldn't be in the interface, but it's here
+     * to make testing nice. Gets an element, recursively. Returns null if
+     * index is out of bounds. Does not alter the deque.
+     *
+     * @param index index to get
+     * @return element at {@code index} in the deque
+     */
+    @Override
+    public T getRecursive(int index) {
+        throw new UnsupportedOperationException("No need to implement getRecursive for ArrayDeque61B.");
+    }
+
+}
--- proj2/tests/ArrayDeque61BTest.java
+++ proj2/tests/ArrayDeque61BTest.java
@@ -1,6 +1,112 @@
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static com.google.common.truth.Truth.assertThat;
 
 public class ArrayDeque61BTest {
+    @Test
+    @DisplayName("Get First Test")
+    void getFirstTest() {
+        ArrayDeque61B<Integer> deque = new ArrayDeque61B<>();
+        assertThat(deque.getFirst()).isEqualTo(null);
+        deque.addFirst(2);
+        assertThat(deque.getFirst()).isEqualTo(2);
+        deque.addFirst(1);
+        assertThat(deque.getFirst()).isEqualTo(1);
+        deque.addLast(3);
+        assertThat(deque.getFirst()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("Get Last Test")
+    void getLastTest() {
+        ArrayDeque61B<Integer> deque = new ArrayDeque61B<>();
+        assertThat(deque.getLast()).isEqualTo(null);
+        deque.addLast(2);
+        assertThat(deque.getLast()).isEqualTo(2);
+        deque.addLast(3);
+        assertThat(deque.getLast()).isEqualTo(3);
+        deque.addFirst(1);
+        assertThat(deque.getLast()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("Get Test")
+    void getTest() {
+        ArrayDeque61B<Integer> deque = new ArrayDeque61B<>();
+        assertThat(deque.get(0)).isEqualTo(null);
+        deque.addFirst(2);
+        assertThat(deque.get(0)).isEqualTo(2);
+        deque.addFirst(1);
+        assertThat(deque.get(0)).isEqualTo(1);
+        deque.addLast(3);
+        assertThat(deque.get(2)).isEqualTo(3);
+
+        assertThat(deque.get(23513515)).isEqualTo(null);
+        assertThat(deque.get(-23513515)).isEqualTo(null);
+    }
+
+    @Test
+    @DisplayName("size Test")
+    void sizeTest() {
+        ArrayDeque61B<Integer> deque = new ArrayDeque61B<>();
+        assertThat(deque.size()).isEqualTo(0);
+        deque.addFirst(2);
+        assertThat(deque.size()).isEqualTo(1);
+        deque.addFirst(1);
+        assertThat(deque.size()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("isEmpty Test")
+    void isEmptyTest() {
+        ArrayDeque61B<Integer> deque = new ArrayDeque61B<>();
+        assertThat(deque.isEmpty()).isTrue();
+        deque.addFirst(2);
+        assertThat(deque.isEmpty()).isFalse();
+    }
+
+    @Test
+    @DisplayName("toList Test")
+    void toListTest() {
+        ArrayDeque61B<Integer> deque = new ArrayDeque61B<>();
+        deque.addFirst(2);
+        deque.addFirst(1);
+        deque.addLast(3);
+        assertThat(deque.toList()).containsExactly(1, 2, 3).inOrder();
+        deque.addFirst(0);
+        assertThat(deque.toList()).containsExactly(0, 1, 2, 3).inOrder();
+    }
+
+    @Test
+    @DisplayName("removeFirst Test")
+    void removeFirstTest() {
+        ArrayDeque61B<Integer> deque = new ArrayDeque61B<>();
+        assertThat(deque.removeFirst()).isEqualTo(null);
+        deque.addFirst(2);
+        assertThat(deque.removeFirst()).isEqualTo(2);
+        assertThat(deque.removeFirst()).isEqualTo(null);
+        deque.addFirst(1);
+        deque.addLast(2);
+        deque.addLast(3);
+        assertThat(deque.removeFirst()).isEqualTo(1);
+        assertThat(deque.toList()).containsExactly(2, 3).inOrder();
+
+    }
+
+    @Test
+    @DisplayName("removeLast Test")
+    void removeLastTest() {
+        ArrayDeque61B<Integer> deque = new ArrayDeque61B<>();
+        assertThat(deque.removeLast()).isEqualTo(null);
+        deque.addFirst(2);
+        assertThat(deque.removeLast()).isEqualTo(2);
+        assertThat(deque.removeLast()).isEqualTo(null);
+        deque.addFirst(2);
+        deque.addFirst(1);
+        deque.addLast(3);
+        assertThat(deque.removeLast()).isEqualTo(3);
+        assertThat(deque.toList()).containsExactly(1, 2).inOrder();
+
+    }
 }
